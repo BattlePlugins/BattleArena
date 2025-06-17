a0eeb73eba24f1c9b07474d83c01d7f487180c01
Ferox <83085562+fenixJK@users.noreply.github.com>
2025-06-17 16:52:44 -0400

    Overhaul
    Support Schematics
    mcedit addtion

--- plugin/src/main/java/org/battleplugins/arena/BattleArenaConfig.java
+++ plugin/src/main/java/org/battleplugins/arena/BattleArenaConfig.java
@@ -30,6 +30,9 @@ public class BattleArenaConfig {
     @ArenaOption(name = "randomized-arena-join", description = "Whether players should be randomly placed in an Arena when joining without specifying a map.", required = true)
     private boolean randomizedArenaJoin;
 
+    @ArenaOption(name = "use-schematic", description = "Whether creating a dynamic arena should try to use a schematic if one is available first.", required = true)
+    private boolean schematicUsage;
+
     @ArenaOption(name = "disabled-modules", description = "Modules that are disabled by default.")
     private List<String> disabledModules;
 
@@ -57,6 +60,10 @@ public class BattleArenaConfig {
 
     public boolean isRandomizedArenaJoin() {
         return this.randomizedArenaJoin;
+    }
+
+    public boolean isSchematicUsage() {
+        return this.schematicUsage;
     }
 
     public List<String> getDisabledModules() {
--- plugin/src/main/java/org/battleplugins/arena/util/BlockUtil.java
+++ plugin/src/main/java/org/battleplugins/arena/util/BlockUtil.java
@@ -5,14 +5,24 @@ import com.sk89q.worldedit.WorldEdit;
 import com.sk89q.worldedit.WorldEditException;
 import com.sk89q.worldedit.bukkit.BukkitAdapter;
 import com.sk89q.worldedit.extent.clipboard.BlockArrayClipboard;
+import com.sk89q.worldedit.extent.clipboard.Clipboard;
+import com.sk89q.worldedit.extent.clipboard.io.BuiltInClipboardFormat;
+import com.sk89q.worldedit.extent.clipboard.io.ClipboardFormat;
+import com.sk89q.worldedit.extent.clipboard.io.ClipboardFormats;
+import com.sk89q.worldedit.extent.clipboard.io.ClipboardReader;
 import com.sk89q.worldedit.function.operation.ForwardExtentCopy;
 import com.sk89q.worldedit.function.operation.Operation;
 import com.sk89q.worldedit.function.operation.Operations;
 import com.sk89q.worldedit.math.BlockVector3;
 import com.sk89q.worldedit.regions.CuboidRegion;
 import com.sk89q.worldedit.session.ClipboardHolder;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.util.Locale;
 import org.battleplugins.arena.BattleArena;
 import org.battleplugins.arena.competition.map.options.Bounds;
+import org.bukkit.Bukkit;
 import org.bukkit.World;
 
 public final class BlockUtil {
@@ -45,5 +55,56 @@ public final class BlockUtil {
         }
 
         return true;
+    }
+
+    public static boolean pasteSchematic(String map, String arena, World world, Bounds bounds) {
+        Path path = BattleArena.getInstance().getDataFolder().toPath()
+                .resolve("schematics")
+                .resolve(arena.toLowerCase(Locale.ROOT))
+                .resolve(map.toLowerCase(Locale.ROOT) + "." +
+                        BuiltInClipboardFormat.SPONGE_SCHEMATIC.getPrimaryFileExtension()
+                );
+
+        if (Files.notExists(path)) {
+            Bukkit.getLogger().warning("Schematic not found: " + path);
+            path = BattleArena.getInstance().getDataFolder().toPath()
+                .resolve("schematics")
+                .resolve(arena.toLowerCase(Locale.ROOT))
+                .resolve(map.toLowerCase(Locale.ROOT) + "." +
+                        BuiltInClipboardFormat.MCEDIT_SCHEMATIC.getPrimaryFileExtension()
+                );
+            if (Files.notExists(path)) {
+                Bukkit.getLogger().warning("Schematic not found: " + path);
+                return false;
+            }
+        }
+
+        ClipboardFormat format = ClipboardFormats.findByFile(path.toFile());
+        if (format == null) {
+            Bukkit.getLogger().warning("Unknown schematic format: " + path.getFileName());
+            return false;
+        }
+
+        Clipboard clipboard;
+        try (ClipboardReader reader = format.getReader(Files.newInputStream(path))) {
+            clipboard = reader.read();
+        } catch (IOException e) {
+            Bukkit.getLogger().severe("Failed to read schematic: " + e.getMessage());
+            return false;
+        }
+
+        try (EditSession session = WorldEdit.getInstance().newEditSession(BukkitAdapter.adapt(world))) {
+            Operation operation = new ClipboardHolder(clipboard)
+                    .createPaste(session)
+                    .to(BlockVector3.at(bounds.getMinX(), bounds.getMinY(), bounds.getMinZ()))
+                    .ignoreAirBlocks(true)
+                    .build();
+
+            Operations.complete(operation);
+            return true;
+        } catch (WorldEditException e) {
+            Bukkit.getLogger().severe("Failed to paste schematic: " + e.getMessage());
+            return false;
+        }
     }
 }
